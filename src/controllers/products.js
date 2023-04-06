e78bf29436e4b48342bf88227ce4c24f99d3ae51
Hebert Zanatelli <hebert_alves1@hotmail.com>
2023-04-06 14:22:01 -0300

    adicionando o metodo put

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,16 +1,16 @@
 const ProductsModel = require('../models/products')
 
-async function get(req,res) {
-    const {id} = req.params
+async function get(req, res) {
+    const { id } = req.params
 
-    let obj = id ? { _id: id} : {} // if ternario (pergunta e resposta na mesma linha, variavel obj tem id? se tiver id cria objeto com _id: id, os dois pontos aseguir é o senao, senão cria obj vazio )
+    let obj = id ? { _id: id } : {} // if ternario (pergunta e resposta na mesma linha, variavel obj tem id? se tiver id cria objeto com _id: id, os dois pontos aseguir é o senao, senão cria obj vazio )
 
     const products = await ProductsModel.find(obj)
-    
+
     res.send(products)
 }
 
-async function post(req, res){
+async function post(req, res) {
     const {
         name,
         brand,
@@ -24,14 +24,29 @@ async function post(req, res){
     })
 
     product.save()
-    
+
     res.send({
         message: 'succes'
     })
 }
 
+async function put(req, res) {
+    const { id } = req.params
+
+    const product = await ProductsModel.findByIdAndUpdate({ _id: id }, req.body, { new: true })
+
+    // const product = await ProductsModel.findOne({ _id:id })
+
+    // await product.updateOne(req.body)
+
+    res.send({
+        message: 'success',
+        product,
+    })
+}
 module.exports = {
     get,
-    post
+    post,
+    put
 }
 
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -9,7 +9,7 @@ const ProductController = require('../controllers/products')
 
 router.get('/products/:id?', ProductController.get) // ao colocar o :id?, determina para o express, que o parametro id é opcional
 router.post('/products', ProductController.post)
-// router.put('/products/:id', ProductController.put)
+router.put('/products/:id', ProductController.put)
 // router.delete('/products/:id', ProductController.delete)
 
 
